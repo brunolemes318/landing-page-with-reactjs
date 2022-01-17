bdb69b92e6f075cf9bfd226a2e0229141b80b3d1
brunolemes318 <brunolemes318@gmail.com>
2022-01-17 15:36:49 -0300

    Estruturado componentes do body layout e adicionado rodapé a landing page

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "../global/header_layout";
-import Body from "../global/body_layout";
-import Footer from "../global/footer_layout";
+import React from "react"
+import Header from "../global/header_layout"
+import Body from "../global/body_layout"
+import Footer from "../global/footer_layout"
 
 export default function App(props) {
-  return (
-    <div>
-      <Header />
-      <Body />
-      <Footer/>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
 }
--- src/components/body/grid_body_component.jsx
+++ src/components/body/grid_body_component.jsx
@@ -3,19 +3,19 @@ import ButtonSocial from "./button_social_body_component"
 import '../../themes/layout/body_theme.scss'
 
 export default function GridTextImagem(props) {
-    
-    if(props.condicao == 1){
+
+    if (props.condicao == 1) {
 
     }
-    
+
     return (
 
         <div className={props.style}>
             <div>
-            {props.children[0]}
+                {props.children[0]}
 
             </div>
-            
+
             <div className=".style-layout-text">
                 {props.children[1]}
                 {props.children[2]}
